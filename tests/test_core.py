5fef8ee2aa0f7f79d0da9842be85c38d336ed492
WangCHEN9 <solid.chen9@gmail.com>
2022-02-01 17:39:27 +0100

    a

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,6 @@
 import pytest
-from sample.core import hi
 
-def test_hi():
-    assert hi().say_hi() == 'Solid'
+# from sample.core import hi
+
+# def test_hi():
+#     assert hi().say_hi() == 'Solid'
